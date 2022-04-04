16d74d6a8e87b921830c3c001dfbe07dd87c42c8
ed.realuyo@gmail.com <ed.realuyo@gmail.com>
2022-04-04 16:04:35 +0800

    remove timer 3, put all off pulse to a single compare match

--- src/main.cpp
+++ src/main.cpp
@@ -15,66 +15,47 @@ void setup() {
   //sets PA0 to PA2 (pin 22-24) as output
   DDRA |= B00000111;
 
-  //sets timer 3
-  TCCR3A = 0;
-  TCCR3B = 0;
-  TCCR3B |= B00000010;  //set prescaler to 8
-  TIMSK3 |= B00000110;  //enable compare match 3A, 3B
-
   //sets timer 4
   TCCR4A = 0;
   TCCR4B = 0;
   TCCR4B |= B00000010;  //set prescaler to 8
-  TIMSK4 |= B00000110;  //enable compare match 4A, 4B
+  TIMSK4 |= B00001110;  //enable compare match 4A, 4B, 4C for heaters
 
   //sets timer 5
   TCCR5A = 0;
   TCCR5B = 0;
   TCCR5B |= B00000010;  //set prescaler to 8
-  TIMSK5 |= B00000110;  //enable compare match 5A, 5B
+  TIMSK5 |= B00000010;  //enable compare match 5A for timer reset
 
   //firing delays rising edge
-  OCR3A = pulse_delay_1;
-  OCR4A = pulse_delay_2;
-  OCR5A = pulse_delay_3;
+  OCR4A = pulse_delay_1;
+  OCR4B = pulse_delay_2;
+  OCR4C = pulse_delay_3;
 
   //firing delays falling edge
-  OCR3B = pulse_delay_max;
-  OCR4B = pulse_delay_max;
-  OCR5B = pulse_delay_max;
+  OCR5A = pulse_delay_max;
   
 
   sei();  //continue interrupts
 }
 
-//firing pulse for heater 1
+
 //turns on firing pulse for heater 1
-ISR(TIMER3_COMPA_vect){
+ISR(TIMER4_COMPA_vect){
   PORTA |= B00000001;
 }
-//turns off firing pulse for heater 1
-ISR(TIMER3_COMPB_vect){
-  PORTA &= !B00000001;                //**********can have all the pins falling edge (PORTA &= !B00000111)
-}
-
-//firing pulse for heater 2
 //turns on firing pulse for heater 2
-ISR(TIMER4_COMPA_vect){
+ISR(TIMER4_COMPB_vect){
   PORTA |= B00000010;
 }
-//turns off firing pulse for heater 2
-ISR(TIMER4_COMPB_vect){
-  PORTA &= !B00000010;
-}
-
-//firing pulse for heater 3
 //turns on firing pulse for heater 3
-ISR(TIMER5_COMPA_vect){
+ISR(TIMER4_COMPB_vect){
   PORTA |= B00000100;
 }
-//turns off firing pulse for heater 3
-ISR(TIMER5_COMPB_vect){
-  PORTA &= !B00000100;
+
+//turns off firing pulse for heater 1,2,3
+ISR(TIMER5_COMPA_vect){
+  PORTA &= !B00000111; 
 }
 
 
