ac90ad3f557d9de6efe45f25581b3d9b7f3c002d
RealED-dev <ed.realuyo@gmail.com>
2022-04-12 23:49:09 +0800

    added function compute in ACHeater.h & .cpp

--- include/ACHeater.h
+++ include/ACHeater.h
@@ -6,10 +6,9 @@
 
 class ACHeater{
     public:
-        ACHeater(double, double, double,
-                    double, double, double);
+        ACHeater(double, double, double, double, bool);
 
-
+        bool PID_Direction;     //true for reversed, false for direct
         double Temp;            //PID input
         double Pulse_Delay;     //PID output
         double Set_Temp;        //PID setpoint  
@@ -17,6 +16,17 @@ class ACHeater{
         double kP;
         double kI;
         double kD;
+
+        double PID_P;
+        double PID_I = 0;
+        double PID_D;
+
+        double Error_Previous;
+
+        unsigned int Delay_Min;
+        unsigned int Delay_Max;
+
+        void Compute(unsigned int);
 };
 
 #endif
--- src/ACHeater.cpp
+++ src/ACHeater.cpp
@@ -2,15 +2,52 @@
 #include "ACHeater.h"
 
 
-ACHeater::ACHeater(double temp, double pulse_delay, double set_temp,
-                    double constP, double constI, double constD)
+ACHeater::ACHeater(double set_temp, double constP, double constI, double constD, bool direction)
 {
-    Temp = temp;
-    Pulse_Delay = pulse_delay;
     Set_Temp = set_temp;
     kP = constP;
     kI = constI;
     kD = constD;
- 
+    PID_Direction = direction;
 }
 
+void ACHeater::Compute(unsigned int Compute_Delay)      //Compute_Delay unit is in ms
+{
+    double Error, PID_value;
+
+    Error = Set_Temp - Temp;
+    //disable Integral when Error is high
+    if(Error > 30)
+    {
+        PID_I = 0;
+    }
+
+    //individually compute for P, I, and D
+    PID_P = kP * Error;
+    PID_I = PID_I + (kI * Error);
+    PID_D = kD * ( (Error - Error_Previous) / (Compute_Delay * 1000) );
+
+    //PID total
+    PID_value = PID_P + PID_I + PID_D;
+    
+    //set PID value only within the specified range
+    if(PID_value > Delay_Max)
+    {
+        PID_value = Delay_Max;
+    }
+    else if (PID_value < Delay_Min)
+    {
+        PID_value = Delay_Min;
+    }
+    
+    //reversed direction for heater control
+    if(PID_Direction)
+    {
+        Pulse_Delay = (Delay_Max + 1) - PID_value;        
+    }
+    else
+    {
+        Pulse_Delay = PID_value;
+    }
+
+}
